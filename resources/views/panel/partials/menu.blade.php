a6051dcc99ee7c4b6725d85c9d606850bc6ac4ad
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2021-07-04 14:46:33 -0400

    actualizacion de plantilla

--- resources/views/panel/partials/menu.blade.php
+++ resources/views/panel/partials/menu.blade.php
@@ -11,23 +11,27 @@
             <li class="nav-item dropdown active">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                    <li class="active"><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                    <li><a class="nav-link" href="index-0.html">Tienda</a></li>
+                    <li class="active"><a class="nav-link" href="index.html">Blog</a></li>
+                    <li class="active"><a class="nav-link" href="index.html">Usuarios</a></li>
+
                 </ul>
             </li>
-            <li class="menu-header">Starter</li>
+            <li class="menu-header">Tienda</li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                    <span>Layout</span></a>
+                    <span>Articulos</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                    <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+                    <li><a class="nav-link" href="layout-default.html">Inventario</a></li>
+                    <li><a class="nav-link" href="layout-transparent.html">Crear Articulo</a></li>
                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
                 </ul>
             </li>
-            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank
+                        Page</span></a></li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
+                    <span>Bootstrap</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
                     <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
@@ -53,16 +57,19 @@
             </li>
             <li class="menu-header">Stisla</li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
+                    <span>Components</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="components-article.html">Article</a></li>
                     <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>
                     <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>
-                    <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>
+                    <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty
+                            State</a></li>
                     <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
                     <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>
                     <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
-                    <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>
+                    <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a>
+                    </li>
                     <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>
                     <li><a class="nav-link" href="components-tab.html">Tab</a></li>
                     <li><a class="nav-link" href="components-table.html">Table</a></li>
@@ -71,7 +78,8 @@
                 </ul>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i>
+                    <span>Forms</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
                     <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
@@ -93,7 +101,8 @@
                 </ul>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i>
+                    <span>Modules</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
                     <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
@@ -121,7 +130,8 @@
                 </ul>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i>
+                    <span>Errors</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="errors-503.html">503</a></li>
                     <li><a class="nav-link" href="errors-403.html">403</a></li>
@@ -130,7 +140,8 @@
                 </ul>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i>
+                    <span>Features</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="features-activities.html">Activities</a></li>
                     <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
@@ -142,15 +153,16 @@
                 </ul>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
+                    <span>Utilities</span></a>
                 <ul class="dropdown-menu">
                     <li><a href="utilities-contact.html">Contact</a></li>
                     <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
                     <li><a href="utilities-subscribe.html">Subscribe</a></li>
                 </ul>
             </li>
-            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a>
-            </li>
+            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i>
+                    <span>Credits</span></a></li>
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
