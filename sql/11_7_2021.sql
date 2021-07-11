64d268e819e4208ca48c442cd3e8bbc3a94d276c
Albert Charmelo <albertcharmelo@gmail.com>
2021-07-11 19:11:36 -0400

    Base de datos al 11/07/2021

---
+++ sql/11_7_2021.sql
@@ -0,0 +1,293 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando estructura para tabla devian.categorias
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_padre` bigint(20) unsigned NOT NULL,
+  `codigo` varchar(4) CHARACTER SET latin1 NOT NULL,
+  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `descripcion` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `imagen` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`),
+  KEY `codigo` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.clientes
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_documento` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `num_documento` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `clientes_nombre_unique` (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.combos
+CREATE TABLE IF NOT EXISTS `combos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.combo_has_producto
+CREATE TABLE IF NOT EXISTS `combo_has_producto` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `combo_id` bigint(20) DEFAULT NULL,
+  `producto_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.compras
+CREATE TABLE IF NOT EXISTS `compras` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `proveedor_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `tipo_identificacion` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `num_compra` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_compra` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total` decimal(15,2) NOT NULL,
+  `estado` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `compras_proveedor_id_foreign` (`proveedor_id`),
+  KEY `compras_user_id_foreign` (`user_id`),
+  CONSTRAINT `compras_proveedor_id_foreign` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores` (`id`),
+  CONSTRAINT `compras_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.detalle_compras
+CREATE TABLE IF NOT EXISTS `detalle_compras` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `compra_id` bigint(20) unsigned NOT NULL,
+  `producto_id` bigint(20) unsigned NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(15,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `detalle_compras_compra_id_foreign` (`compra_id`),
+  KEY `detalle_compras_producto_id_foreign` (`producto_id`),
+  CONSTRAINT `detalle_compras_compra_id_foreign` FOREIGN KEY (`compra_id`) REFERENCES `compras` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `detalle_compras_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.detalle_ventas
+CREATE TABLE IF NOT EXISTS `detalle_ventas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `venta_id` bigint(20) unsigned NOT NULL,
+  `producto_id` bigint(20) unsigned NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(15,2) NOT NULL,
+  `descuento` decimal(15,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `detalle_ventas_producto_id_foreign` (`producto_id`),
+  KEY `detalle_ventas_venta_id_foreign` (`venta_id`),
+  CONSTRAINT `detalle_ventas_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`),
+  CONSTRAINT `detalle_ventas_venta_id_foreign` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.lista_deseos
+CREATE TABLE IF NOT EXISTS `lista_deseos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `producto_id` bigint(20) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lista_user_id_foreign` (`user_id`),
+  KEY `lista_producto_id_foreign` (`producto_id`),
+  CONSTRAINT `lista_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `lista_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Tabla para añadir productos a lista de deseos para un determinado usuario.';
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.plantillas
+CREATE TABLE IF NOT EXISTS `plantillas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `categoria_id` bigint(20) unsigned NOT NULL,
+  `nombre` varchar(50) NOT NULL DEFAULT '',
+  `descripcion` longtext NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plantilla_categoria_id__foreign` (`categoria_id`),
+  CONSTRAINT `plantilla_categoria_id__foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Se especifica los datelles o atributos que pueda tener un producto haciendo referencia al id de la categoría del procducto.';
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `categoria_id` bigint(20) unsigned NOT NULL,
+  `referencia` varchar(10) CHARACTER SET latin1 NOT NULL,
+  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `descripcion` longtext COLLATE utf8mb4_unicode_ci,
+  `stock` int(8) unsigned NOT NULL,
+  `precio` decimal(15,2) DEFAULT NULL,
+  `tipo_unidad` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
+  `garantia` int(5) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT '1',
+  `atributo` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`),
+  KEY `categoria_id` (`categoria_id`),
+  KEY `codigo` (`referencia`),
+  CONSTRAINT `productos_categoria_id_foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.productos_similar_comparar
+CREATE TABLE IF NOT EXISTS `productos_similar_comparar` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `producto_id` bigint(20) unsigned DEFAULT NULL,
+  `tipo` varchar(1) NOT NULL DEFAULT 'C',
+  `producto_id_cs` bigint(20) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_sc_producto_id_cs__foreign` (`producto_id_cs`),
+  KEY `producto_sc_producto_id_foreign` (`producto_id`),
+  CONSTRAINT `producto_sc_producto_id_cs__foreign` FOREIGN KEY (`producto_id_cs`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `producto_sc_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='tabla para referenciar id de producto y hacer  comparaciones con otros productos de la misma gama o intercambiables. Tambièn para hacer sugerencias de productos similares o accesorios. el atributo TIPO indicara si es comparaciòn o sugerencia. C/S';
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.proveedores
+CREATE TABLE IF NOT EXISTS `proveedores` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_documento` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `num_documento` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `proveedores_nombre_unique` (`nombre`),
+  KEY `num_documento` (`num_documento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ciudad` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provincia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.valoraciones
+CREATE TABLE IF NOT EXISTS `valoraciones` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `producto_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `fecha` timestamp NOT NULL,
+  `puntos` int(1) unsigned NOT NULL,
+  `texto` varchar(250) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `valoracion_user_id__foreign` (`user_id`),
+  KEY `valoracion_producto_id_foreign` (`producto_id`),
+  CONSTRAINT `valoracion_producto_id_foreign` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `valoracion_user_id__foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla devian.ventas
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `cliente_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `tipo_identificacion` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `num_venta` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_venta` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total` decimal(15,2) NOT NULL,
+  `estado` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ventas_cliente_id_foreign` (`cliente_id`),
+  KEY `ventas_user_id_foreign` (`user_id`),
+  CONSTRAINT `ventas_cliente_id_foreign` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
+  CONSTRAINT `ventas_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
