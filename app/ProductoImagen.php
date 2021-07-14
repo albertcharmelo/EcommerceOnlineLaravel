60b408d26b5d6a5798b80318c2860235ec053945
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2021-07-13 20:14:08 -0400

    guardar imaganes

---
+++ app/ProductoImagen.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductoImagen extends Model
+{
+    protected $table = 'producto_has_image';
+
+    protected $guarded = [];
+}
