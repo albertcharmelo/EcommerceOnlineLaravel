5723bfd8991fa480ccfdbc43160e2710d7626616
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2021-08-09 19:54:32 -0400

    Arreglo menu

--- resources/views/panel/partials/menu.blade.php
+++ resources/views/panel/partials/menu.blade.php
@@ -19,7 +19,7 @@
                     {{-- <li><a class="nav-link"  href="{{ url('/panel/dashboard/usuarios') }}">Usuarios</a>
             </li> --}}
 
-                    <<<<<<< Updated upstream </ul>
+                     </ul>
             </li>
             <li class="nav-item dropdown ">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-users"></i><span>Administración</span></a>
@@ -63,59 +63,67 @@
             </li>
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-clipboard-list "></i> <span>Reportes</span></a> --}}
             </li>
-            
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
+                    <span>Inventario</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/inventario/index') }}">Generar Inventario</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Tienda</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-mobile-alt"></i>
+                    <span>Almacén</span></a>
+                <ul class="dropdown-menu">
+                    {{-- <li><a class="nav-link" href="layout-default.html">Inventario</a></li> --}}
+                    <li><a class="nav-link" href="{{ url('/panel/articulos/create/articulos') }}">Productos</a></li>
+                    <li><a class="nav-link" href="{{ url('/panel/articulos/create/categoria') }}">Categorías</a></li>
+                    <li><a class="nav-link" href="{{ url('') }}">Unidades de Medida</a></li>
+                    {{-- <li><a class="nav-link" href="layout-top-navigation.html">Crear Etiquetas</a></li> --}}
+    
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
+                    <span>Blog</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/blog/create/post') }}">Crear
+                            Noticias</a></li>
+                    <li><a class="nav-link" href="{{ url('/panel/blog/create/categoria') }}">Crear
+                            Categoria</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
+                    <span>Compras</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/compras/index') }}">Ingresos</a></li>
+                </ul>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/proveedores/index') }}">Proveedores</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
+                    <span>Inventario</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/inventario/index') }}">Generar Inventario</a></li>
+                </ul>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ url('/panel/inventario/index') }}">Inventario Valorizado</a></li>
+                </ul>
+    
+                </ul>
+    
+                <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                    <a href="#" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                        <i class="fas fa-rocket"></i> Ir a la Tienda
+                    </a>
+                </div>
+            </li>
         </ul>
-        </li>
+       
 
-        <li class="menu-header">Tienda</li>
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-mobile-alt"></i>
-                <span>Almacén</span></a>
-            <ul class="dropdown-menu">
-                {{-- <li><a class="nav-link" href="layout-default.html">Inventario</a></li> --}}
-                <li><a class="nav-link" href="{{ url('/panel/articulos/create/articulos') }}">Productos</a></li>
-                <li><a class="nav-link" href="{{ url('/panel/articulos/create/categoria') }}">Categorías</a></li>
-                <li><a class="nav-link" href="{{ url('') }}">Unidades de Medida</a></li>
-                {{-- <li><a class="nav-link" href="layout-top-navigation.html">Crear Etiquetas</a></li> --}}
-
-            </ul>
-        </li>
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
-                <span>Blog</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ url('/panel/blog/create/post') }}">Crear
-                        Noticias</a></li>
-                <li><a class="nav-link" href="{{ url('/panel/blog/create/categoria') }}">Crear
-                        Categoria</a></li>
-            </ul>
-        </li>
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
-                <span>Compras</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ url('/panel/compras/index') }}">Ingresos</a></li>
-            </ul>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ url('/panel/proveedores/index') }}">Proveedores</a></li>
-            </ul>
-        </li>
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
-                <span>Inventario</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ url('/panel/inventario/index') }}">Generar Inventario</a></li>
-            </ul>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="{{ url('/panel/inventario/index') }}">Inventario Valorizado</a></li>
-            </ul>
-
-            </ul>
-
-            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                <a href="#" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                    <i class="fas fa-rocket"></i> Ir a la Tienda
-                </a>
-            </div>
+       
     </aside>
 </div>
