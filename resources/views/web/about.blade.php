8fc65d2e07b2fe8be1045013499d5eb016fb7a93
albertcharmelocontacto@gmail.com <albertcharmelocontacto@gmail.com>
2021-09-15 08:45:21 -0400

    fixes

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -4,7 +4,7 @@
 <section class="bg0 p-t-75 p-b-120">
     <div class="container">
         <div class="row p-b-148">
-            <div class="col-md-7 col-lg-7">
+            <div class="col-md-7 col-12 col-lg-7">
                 <div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
                     <h3 class="mtext-111 cl2 p-b-16">
                         Nuestra Historia
@@ -20,7 +20,7 @@
                 </div>
             </div>
 
-            <div class="col-11 col-md-5 col-lg-5 m-lr-auto">
+            <div class="col-12 col-md-5 col-lg-5 m-lr-auto">
                 <div class="how-bor1 ">
                     <div class="hov-img0">
                         <img src="{{ asset('assets/img/Bonao_logo.png') }}" width="300px" alt="bonao">
